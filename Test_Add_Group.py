014908f41e1d71f4e6008395e5d505736f82b2df
iluha <radjah.905@gmail.com>
2023-02-15 17:21:09 +0300

    Рефакторинг по методам

--- Test_Add_Group.py
+++ Test_Add_Group.py
@@ -13,15 +13,27 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        # login
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        # open groups page
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -36,14 +48,12 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("rttttt")
         # submit group creation
         wd.find_element_by_name("submit").click()
-        # return to groups page
-        wd.find_element_by_link_text("groups").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
 
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/")
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
