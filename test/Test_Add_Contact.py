c1db77ef08a21bb3dfd5223c2bbe1307ae5fab34
iluha <radjah.905@gmail.com>
2023-03-02 19:15:03 +0300

    Оптимизирован ввод данных, выделены доп. методы для переиспользования кода, login перенесён в conftest

--- test/Test_Add_Contact.py
+++ test/Test_Add_Contact.py
@@ -2,7 +2,6 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_new_contact(Contact(firstname="Test", middlename="Test", lastname="Test", nickname="wow", file="C:\Games\LEtXJtJk.jpg", title="title", company="company",
                                 address="11 22 33", home="444555666", mobile="89992224455", work="787565", fax="1456", email="test@mail.kek",
                                 email2="test1@mail.kek", email3="test2@mail.kek", homepage="homepage", bday="13", bmonth="January", byear="1900", aday="29", amonth="May",
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,7 +1,6 @@
 from model.contact import Contact
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="123", middlename="123", lastname="123", nickname="123", file="C:\Games\LEtXJtJk.jpg", title="123", company="123",
                                 address="123", home="123", mobile="123", work="123", fax="123", email="123",
                                 email2="123", email3="123", homepage="123", bday="11", bmonth="April", byear="2000", aday="26", amonth="July",
