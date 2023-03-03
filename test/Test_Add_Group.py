880bbae6363789b6f89cc746836feb6db500e804
iluha <radjah.905@gmail.com>
2023-03-03 18:35:19 +0300

    Валидация фикстуры перед каждым тестом, выделен is_valid, finalizer(fin)

--- test/Test_Add_Group.py
+++ test/Test_Add_Group.py
@@ -5,5 +5,4 @@ def test_add_group(app):
     app.group.create(Group(name="fgtgyhht", header="eereee", footer="rttttt"))
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
