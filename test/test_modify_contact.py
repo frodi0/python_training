ce4cc37709fb3fc9151c4604ed797a2dcfe9ebd5
iluha <radjah.905@gmail.com>
2023-03-09 19:57:37 +0300

    Закомментировал test_modify_birthday_and_anniversary для дальнейшей оптимизации

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -19,4 +19,11 @@ def test_modify_first_contact(app):
 #def test_modify_birthday_and_anniversary(app):
     #if app.contact.count() == 0:
         #app.contact.create_new_contact(Contact(firstname='test'))
-    #app.contact.modify_first_contact(Contact(bday="12", bmonth="July", byear="2010", aday="14", amonth="June", ayear="1980"))
+    #old_contacts = app.contact.get_contact_list()
+    #contact = Contact(bday="12", bmonth="July", byear="2010", aday="14", amonth="June", ayear="1980")
+    #contact.id = old_contacts[0].id
+   # app.contact.modify_first_contact(contact)
+    #new_contacts = app.contact.get_contact_list()
+    #assert len(old_contacts) == len(new_contacts)
+   # old_contacts[0] = contact
+    #assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
