54f830af68df2e001af802c37be0f8e8a7d8ccd4
iluha <radjah.905@gmail.com>
2023-03-09 19:47:14 +0300

    Добавлены проверки по сравнению списков во всех тестах (группы и контакты), добавлена операция get_contact_list. Тесты test_modify_group_name и test_modify_group_header закомментированы для дальнейшей оптимизации

--- test/Test_Add_Contact.py
+++ test/Test_Add_Contact.py
@@ -2,7 +2,13 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.contact.create_new_contact(Contact(firstname="Test", middlename="Test", lastname="Test", nickname="wow", file="C:\Games\LEtXJtJk.jpg", title="title", company="company",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="Test", middlename="Test", lastname="Test", nickname="wow", file="C:\Games\LEtXJtJk.jpg", title="title", company="company",
                                 address="11 22 33", home="444555666", mobile="89992224455", work="787565", fax="1456", email="test@mail.kek",
                                 email2="test1@mail.kek", email3="test2@mail.kek", homepage="homepage", bday="13", bmonth="January", byear="1900", aday="29", amonth="May",
-                                ayear="2000", address2="Wowcity, Wowstreet, 55", phone2="yes", notes="none"))
+                                ayear="2000", address2="Wowcity, Wowstreet, 55", phone2="yes", notes="none")
+    app.contact.create_new_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,12 +3,20 @@ from model.contact import Contact
 def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create_new_contact(Contact(firstname='test'))
-    app.contact.modify_first_contact(Contact(firstname="123", middlename="123", lastname="123", nickname="123", file="C:\Games\LEtXJtJk.jpg", title="123", company="123",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="123", middlename="123", lastname="123", nickname="123", file="C:\Games\LEtXJtJk.jpg", title="123", company="123",
                                 address="123", home="123", mobile="123", work="123", fax="123", email="123",
                                 email2="123", email3="123", homepage="123", bday="11", bmonth="April", byear="2000", aday="26", amonth="July",
-                                ayear="1990", address2="123", phone2="123", notes="123"))
+                                ayear="1990", address2="123", phone2="123", notes="123")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-def test_modify_birthday_and_anniversary(app):
-    if app.contact.count() == 0:
-        app.contact.create_new_contact(Contact(firstname='test'))
-    app.contact.modify_first_contact(Contact(bday="12", bmonth="July", byear="2010", aday="14", amonth="June", ayear="1980"))
+
+#def test_modify_birthday_and_anniversary(app):
+    #if app.contact.count() == 0:
+        #app.contact.create_new_contact(Contact(firstname='test'))
+    #app.contact.modify_first_contact(Contact(bday="12", bmonth="July", byear="2010", aday="14", amonth="June", ayear="1980"))
