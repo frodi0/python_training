dc1babb7685d5ac2dae7e6c915155dd26d1414b4
iluha <radjah.905@gmail.com>
2023-03-09 17:30:57 +0300

    Добавлены проверки списков во все тесты, связанные с группами

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,14 +4,23 @@ from model.group import Group
 def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="111", header="222", footer="333"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
