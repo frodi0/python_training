9c7556bd878679863bb4862eaddcd6fe52c8d41f
iluha <radjah.905@gmail.com>
2023-03-27 22:24:49 +0300

    Добавлен тест test_check_contact_info для проверки информации по случайно выбранному контакту

--- fixture/contact.py
+++ fixture/contact.py
@@ -131,13 +131,15 @@ class ContactHelper:
             self.contact_cache = []
             for element in wd.find_elements_by_name("entry"):
                 cells = element.find_elements_by_tag_name("td")
-                firstname = cells[1].text
-                lastname = cells[2].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
-                                                  homephone=all_phones[0], mobilephone=all_phones[1],
-                                                  workphone=all_phones[2], secondaryphone=all_phones[3]))
+                lastname = cells[1].text
+                firstname = cells[2].text
+                address = cells[3].text
+                all_emails = cells[4].text
+                all_phones = cells[5].text
+
+                self.contact_cache.append(Contact(id=id, lastname=lastname, firstname=firstname, address=address,
+                                                  all_emails_homepage=all_emails, all_phones_from_homepage=all_phones))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -157,15 +159,20 @@ class ContactHelper:
     def get_contact_info_from_edit_page(self, index):
         wd = self.app.wd
         self.open_contact_to_edit_by_index(index)
-        firstname = wd.find_element_by_name("firstname").get_attribute("value")
-        lastname = wd.find_element_by_name("lastname").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
         homephone = wd.find_element_by_name("home").get_attribute("value")
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id=id,
-                       homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
+        return Contact(id=id, lastname=lastname, firstname=firstname, address=address,
+                       email=email, email2=email2, email3=email3,
+                       homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
@@ -175,4 +182,4 @@ class ContactHelper:
         workphone = re.search("W: (.*)", text).group(1)
         mobilephone = re.search("M: (.*)", text).group(1)
         secondaryphone = re.search("P: (.*)", text).group(1)
-        return Contact(homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
+        return Contact(homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,12 +1,10 @@
 import re
+from random import randrange
 
 def test_phones_on_homepage(app):
     contact_from_homepage = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_homepage.homephone == clear(contact_from_edit_page.homephone)
-    assert contact_from_homepage.workphone == clear(contact_from_edit_page.workphone)
-    assert contact_from_homepage.mobilephone == clear(contact_from_edit_page.mobilephone)
-    assert contact_from_homepage.secondaryphone == clear(contact_from_edit_page.secondaryphone)
+    assert contact_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
@@ -16,5 +14,27 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
 
+def test_check_contact_info(app):
+    contact_from_homepage = app.contact.get_contact_list()
+    index = randrange(len(contact_from_homepage))
+    random_contact_homepage = app.contact.get_contact_list()[index]
+    random_contact_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert random_contact_homepage.id == random_contact_edit_page.id
+    assert random_contact_homepage.lastname == random_contact_edit_page.lastname
+    assert random_contact_homepage.firstname == random_contact_edit_page.firstname
+    assert random_contact_homepage.address == random_contact_edit_page.address
+    assert random_contact_homepage.all_emails_homepage == merge_email_like_on_homepage(random_contact_edit_page)
+    assert random_contact_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(random_contact_edit_page)
+
 def clear(s):
     return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+
+def merge_email_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "", [contact.email, contact.email2, contact.email3]))
+
