e7edcd563fe4156e118909c17c7e834f659fd325
iluha <radjah.905@gmail.com>
2023-03-27 19:38:30 +0300

    Добавлен test_phones_on_contact_view_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.select import Select
 from model.contact import Contact
+import re
 
 class ContactHelper:
 
@@ -166,4 +167,12 @@ class ContactHelper:
         return Contact(firstname=firstname, lastname=lastname, id=id,
                        homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
 
-
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,10 +1,20 @@
-
+import re
 
 def test_phones_on_homepage(app):
     contact_from_homepage = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_homepage.homephone == contact_from_edit_page.homephone
-    assert contact_from_homepage.workphone == contact_from_edit_page.workphone
-    assert contact_from_homepage.mobilephone == contact_from_edit_page.mobilephone
-    assert contact_from_homepage.secondaryphone == contact_from_edit_page.secondaryphone
+    assert contact_from_homepage.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_homepage.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_homepage.mobilephone == clear(contact_from_edit_page.mobilephone)
+    assert contact_from_homepage.secondaryphone == clear(contact_from_edit_page.secondaryphone)
 
+def test_phones_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+
+def clear(s):
+    return re.sub("[() -]", "", s)
