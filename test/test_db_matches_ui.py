7892838c393b0e09f36f022bbeb00fd3adfda897
iluha <radjah.905@gmail.com>
2023-04-09 17:43:34 +0300

    Обновлен test_add_contact для сравнения данных из БД

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,8 +1,17 @@
 from model.group import Group
+from model.contact import Contact
+from timeit import timeit
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
+    print(timeit(lambda: app.group.get_group_list(), number=1))
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
-    db_list = map(clean, db.get_group_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
+    ui_group_list = app.group.get_group_list()
+    db_group_list = db.get_group_list()
+    assert sorted(ui_group_list, key=Group.id_or_max) == sorted(db_group_list, key=Group.id_or_max)
+
+def test_contact_list(app, db):
+    ui_contact_list = app.contact.get_contact_list()
+    db_contact_list = db.get_contact_list()
+    assert sorted(ui_contact_list, key=Contact.id_or_max) == sorted(db_contact_list, key=Contact.id_or_max)
